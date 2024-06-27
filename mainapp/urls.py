4f7fa0ce03cbf68601c64460e0a24ca25e4e46d5
legostaev <alexey.legostaev@gmail.com>
2024-06-27 14:39:50 +0300

    добавил форму редактирования рецепта, добаил сслку на шаблон редатирования в шаблон просмотра рецепта

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import MainRecipeListView, RecipeDetailView, RegisterView, RegistrationSuccessView, LogoutSuccessView, AllRecipesView
+from .views import MainRecipeListView, RecipeDetailView, RegisterView, RegistrationSuccessView, LogoutSuccessView, \
+    AllRecipesView, RecipeCreateView, RecipeUpdateView
 from django.contrib.auth import views as auth_views
 
 app_name = 'mainapp'
@@ -7,12 +8,12 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', MainRecipeListView.as_view(), name='index'),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
-    # path('register/', register, name='register'),
     path('register/', RegisterView.as_view(), name='register'),
     path('registration_success/', RegistrationSuccessView.as_view(), name='registration_success'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout_success/', LogoutSuccessView.as_view(), name='logout_success'),
     path('logout/', auth_views.LogoutView.as_view(next_page='mainapp:logout_success'), name='logout'),
     path('all_recipes/', AllRecipesView.as_view(), name='all_recipes'),
-
+    path('recipe/add/', RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/edit/', RecipeUpdateView.as_view(), name='recipe_edit'),
 ]
